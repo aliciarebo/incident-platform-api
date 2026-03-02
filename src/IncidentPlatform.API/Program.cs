6d7395291dfc2d64d0eb893951d06a4c141c99a3
alicia <alicia.re.sanchez@gmail.com>
2026-03-02 08:58:02 +0100

    feat(infrastructure): add in-memory incident repository

--- src/IncidentPlatform.API/Program.cs
+++ src/IncidentPlatform.API/Program.cs
@@ -1,3 +1,6 @@
+using IncidentPlatform.Application.Incidents.Ports;
+using IncidentPlatform.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Swagger (OpenAPI)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IIncidentRepository, InMemoryIncidentRepository>();
 
 var app = builder.Build();
 
