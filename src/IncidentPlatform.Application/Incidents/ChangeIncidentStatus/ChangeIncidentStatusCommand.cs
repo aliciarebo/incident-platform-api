deb4bfc6c31397fad348bb645b4abb54c504c48a
alicia <alicia.re.sanchez@gmail.com>
2026-04-23 15:39:32 +0200

    feat(change-status): implement change incident status flow with permissions and workflow rules

---
+++ src/IncidentPlatform.Application/Incidents/ChangeIncidentStatus/ChangeIncidentStatusCommand.cs
@@ -0,0 +1,15 @@
+﻿using IncidentPlatform.Domain.Incidents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncidentPlatform.Application.Incidents.ChangeIncidentStatus
+{
+    public sealed record ChangeIncidentStatusCommand(
+    Guid IncidentId,
+    IncidentStatus Status
+    );
+
+}
