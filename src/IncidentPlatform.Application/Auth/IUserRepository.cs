cf451003c08604627c3811b56e6f12e833bbfeee
alicia <alicia.re.sanchez@gmail.com>
2026-04-30 09:33:15 +0200

    feat(auth-login):add auth ports for user repository and token service

---
+++ src/IncidentPlatform.Application/Auth/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using IncidentPlatform.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncidentPlatform.Application.Auth
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAsync(string email);
+    }
+}
